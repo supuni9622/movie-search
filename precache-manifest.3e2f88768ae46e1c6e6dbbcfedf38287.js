5a76424eaaceb70de134f5872fe46033a998f060
supuni9622 <supunimanamperi@gmail.com>
2020-08-18 09:46:11 +0530

    Updates

---
+++ precache-manifest.3e2f88768ae46e1c6e6dbbcfedf38287.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "474a290e4ac5644623397a5d58c41020",
+    "url": "/movie-search/index.html"
+  },
+  {
+    "revision": "8d4e471d629d83a5120e",
+    "url": "/movie-search/static/css/main.88bf66f4.chunk.css"
+  },
+  {
+    "revision": "998877c0d53551b23547",
+    "url": "/movie-search/static/js/2.957f8e12.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/movie-search/static/js/2.957f8e12.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8d4e471d629d83a5120e",
+    "url": "/movie-search/static/js/main.f0b7263b.chunk.js"
+  },
+  {
+    "revision": "8f1314dfdc21c95c9344",
+    "url": "/movie-search/static/js/runtime-main.8345f5b5.js"
+  }
+]);
